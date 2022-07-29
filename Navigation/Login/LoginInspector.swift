c5c9a4637c43c299cc4150aec2b50505e4af6f59
Lazarev Egor <lazarev_es@sima-land.ru>
2022-07-29 13:47:43 +0500

    Выполнил дз 9

--- Navigation/Login/LoginInspector.swift
+++ Navigation/Login/LoginInspector.swift
@@ -4,22 +4,25 @@
 //
 //  Created by Егор Лазарев on 30.06.2022.
 //
-
 import Foundation
 
 protocol LoginViewControllerDelegate: AnyObject {
     func signIn(login: String, password: String, callback: @escaping (_ result: (Result<Bool, AppError>), _ message: String) -> Void)
     func registerIn(login: String, password: String, callback: @escaping (_ result: (Result<Bool, AppError>), _ message: String) -> Void)
+    func checkUserToDataBase(callback: @escaping (_ user: User)->Void)
 }
 
 class LoginInspector: LoginViewControllerDelegate  {
     
     var callback: ((Result<Bool, AppError>, _ message: String) -> Void)?
- 
+    var callbackDataBase: ((_ user: User)->Void)?
+    let dataBaseCoordinator: RealmDatabaseCoordinatable = RealmCoordinator()
+    
     func signIn(login: String, password: String, callback: @escaping ((Result<Bool, AppError>), _ message: String) -> Void) {
         self.callback = callback
         Checker.shared.checkUserData(checkLogin: login, checkPassword: password) { result, message in
             if result {
+                self.saveUser(login: login, password: password)
                 self.succsesSignIn()
             } else {
                 self.failedSignIn(message: message)
@@ -32,10 +35,28 @@ class LoginInspector: LoginViewControllerDelegate  {
         self.callback = callback
         Checker.shared.registerUserData(Login: login, Password: password) { result, message in
             if result {
+                self.saveUser(login: login, password: password)
                 self.succsesRegisterIn()
             } else {
                 self.failedRegisterIn(message: message)
                 
+            }
+        }
+    }
+    
+    func checkUserToDataBase(callback: @escaping (_ user: User)->Void) {
+        callbackDataBase = callback
+        dataBaseCoordinator.fetch(User.self, predicate: nil) { result in
+            switch result {
+            case .success(let UserModels):
+                if UserModels.count > 0 {
+                    if let callbackDataBase = self.callbackDataBase, let _ = UserModels[0].name {
+                        print(UserModels[0])
+                        callbackDataBase(UserModels[0])
+                    }
+                }
+            case .failure:
+                break
             }
         }
     }
@@ -62,6 +83,11 @@ class LoginInspector: LoginViewControllerDelegate  {
         if let callback = callback {
             callback(.success(false), message)
         }
+    }
+    
+    private func saveUser(login: String, password: String) {
+        let user = User(name: login, password: password, status: "", avatarName: "")
+        dataBaseCoordinator.create(User.self, keyedValues: [user.keyedValues])
     }
     
 }
